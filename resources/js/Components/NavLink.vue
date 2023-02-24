13257969660da5a553f84e1fb7ef1c5818a059cb
aden anteng <hey@anteng.co>
2023-02-24 08:15:38 +0700

    idk how

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -22,7 +22,7 @@ const classes = computed(() => {
 </script>
 
 <template>
-    <Link :href="href" :class="classes">
+    <a :href="href" :class="classes">
         <slot />
-    </Link>
+    </a>
 </template>
